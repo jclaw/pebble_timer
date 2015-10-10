b93c14827399bef88dfdb1669f7120b8f0a10caa
Jackson Clawson <jclaw3456@gmail.com>
2015-10-10 14:20:01 -0400

    working button click handlers

--- src/main.c
+++ src/main.c
@@ -1,16 +1,45 @@
 #include <pebble.h>
+//#include "windows/pin_window.h"
 
+  
 static Window *s_main_window;
+static TextLayer *s_output_layer;
 
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_output_layer, "Up pressed!");
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_output_layer, "Select pressed!");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_output_layer, "Down pressed!");
+}
+
+static void click_config_provider(void *context) {
+  // Register the ClickHandlers
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
 
 static void main_window_load(Window *window) {
-  // Create Window's child Layers here
+  
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(0, 55, 144, 55));
+  text_layer_set_background_color(s_output_layer, GColorClear);
+  text_layer_set_text_color(s_output_layer, GColorBlack);
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_output_layer));
 
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy Window's child Layers here
-
+  // Destroy TextLayer
+  text_layer_destroy(s_output_layer);
 }
 
 static void init() {
@@ -21,6 +50,8 @@ static void init() {
     .unload = main_window_unload,
   });
   
+  window_set_click_config_provider(s_main_window, click_config_provider);
+
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 }
